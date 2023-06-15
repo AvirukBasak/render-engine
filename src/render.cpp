8143ca9fbe19dbe621d412ec4ca6cd459d67b634
AvirukBasak <avirukbasak@gmail.com>
2023-06-16 03:37:22 +0530

    na

--- src/render.cpp
+++ src/render.cpp
@@ -16,3 +16,21 @@ void reng::render(Box& box, int width, int height)
         window.display();
     }
 }
+
+void reng::render(Box& box, int width, int height, std::function<void (sf::Window& window)>& callback)
+{
+    sf::RenderWindow window(sf::VideoMode(width, height), "Box Example");
+
+    // Render the Box and its elements
+    box.render(window);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            callback(window);
+        }
+        window.display();
+    }
+}
