857854e0c0deacc8b54c801dbdcbd420a1d0321d
Aviruk Basak <avirukbasak@gmail.com>
2023-06-17 02:20:00 +0530

    Update reng.cpp
    Changes to event loop

--- src/reng.cpp
+++ src/reng.cpp
@@ -5,17 +5,17 @@ using namespace reng;
 void reng::reng(Box box, std::function<void (sf::Window& window)> callback)
 {
     sf::RenderWindow window(sf::VideoMode(box.getWidth(), box.getHeight()), box.getName());
-
-    // Render the Box and its elements
-    box.render(window);
-
+    // event loop
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            // updates callback function
             if (callback) callback(window);
         }
+        window.clear();
+        if (!box.isHidden()) box.render(window);
         window.display();
     }
 }
