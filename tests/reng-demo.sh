fe6eebdb0911a528edf16f17f248e74a32b6a052
AvirukBasak <avirukbasak@gmail.com>
2023-06-16 21:19:13 +0530

    na

--- tests/reng-demo.sh
+++ tests/reng-demo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 g++ -DDEBUG -DLOCAL -g -fsanitize=address -o reng-demo.out reng-demo.cpp -I../include -L../build/lib -lreng -lsfml-graphics -lsfml-window -lsfml-system -lm && ./reng-demo.out
+rm -rf *.out
--- tests/sfml-demo.sh
+++ tests/sfml-demo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 g++ -DDEBUG -DLOCAL -g -fsanitize=address -o sfml-demo.out sfml-demo.cpp -lsfml-graphics -lsfml-window -lsfml-system -lm && ./sfml-demo.out
+rm -rf *.out
